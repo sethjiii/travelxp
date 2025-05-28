33b79492a29af704e3e0126561788cafdf6a2ee9
jay kumar gupta <jaynrk2002@gmail.com>
2025-05-28 08:17:57 +0530

    hey

--- src/pages/api/auth/login.js
+++ src/pages/api/auth/login.js
@@ -7,7 +7,6 @@ export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
-
   try {
     const { email, password } = req.body;
     if (!email || !password) {
